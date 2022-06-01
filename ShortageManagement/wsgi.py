86723d3de1bcaccc2a0e76e0caa7aa2bc6743771
Houssem Edine Selmi <houssemedines@gmail.com>
2022-06-01 08:52:15 +0100

    config file wsgi

--- ShortageManagement/wsgi.py
+++ ShortageManagement/wsgi.py
@@ -8,8 +8,14 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
 
 from django.core.wsgi import get_wsgi_application
+
+
+sys.path.append('C:/shortage_env/shortage_manquant/ShortageManagement')
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ShortageManagement.settings')
 
